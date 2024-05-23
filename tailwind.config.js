d9a53b3bd5613b383c106e42e303973f3eefbc77
Sanchit Pahurkar <pahurkarsm@rknec.edu>
2024-05-23 10:43:32 +0530

    explore section completed (responsive)

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,17 @@ module.exports = {
       },
       colors: {
         'olive-green': '#618D3D',
+        'olive-green-light': '#89ba60',
       },
       backgroundImage: theme =>({
         'breakSec' : "url('/media/breakBGIMG.jpg')",
       }),
+      
+      backgroundImage: {
+        'bandar' : "url('/media/bandar.jpg')",
+        'phuket' : "url('/media/phuket.jpg')",
+        'goa': "url('/media/goa.jpg')",
+      },
     },
   },
   plugins: [],
