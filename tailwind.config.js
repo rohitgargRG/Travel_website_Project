ea19a55900638f87a251beb2b6bd510c800925f2
tanmay-37 <talekartk@rknec.edu>
2024-05-30 13:08:10 +0530

    review section completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist/**/*.{html,js}'],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        lobstertwo: ['Lobster Two', 'sans-serif'],
-        vollkorn: ['Vollkorn', 'serif'],
+        lobstertwo: ["Lobster Two", "sans-serif"],
+        vollkorn: ["Vollkorn", "serif"],
       },
       colors: {
-        'olive-green': '#618D3D',
-        'olive-green-light': '#89ba60',
+        "olive-green": "#618D3D",
+        "olive-green-light": "#89ba60",
+        "review-green": "#CEBA5A",
       },
       // backgroundImage: theme =>({
       //   'breakSec' : "url('/media/breakBGIMG.jpg')",
       // }),
-      
+
       backgroundImage: {
-        'breakSec' : "url('/media/breakBGIMG.jpg')",
-        'bandar' : "url('/media/bandar.jpg')",
-        'phuket' : "url('/media/phuket.jpg')",
-        'goa': "url('/media/goa.jpg')",
-        'package1' : "url('/media/p1.jpg')",
-        'package2' : "url('/media/p_2.jpeg')",
-        'package3' : "url('/media/p3.jpg')",
-        'package4' : "url('/media/p4.jpg')",
-        '5_Star_black' : "url('/media/5-stars_black.png')",
-        'forest' : "url('/media/forest.jpg')",
-        'trees' : "url('/media/tree.jpg')",
-      }
-    }
+        breakSec: "url('/media/breakBGIMG.jpg')",
+        bandar: "url('/media/bandar.jpg')",
+        phuket: "url('/media/phuket.jpg')",
+        goa: "url('/media/goa.jpg')",
+        lesGo: "url('/media/lesGo.jpg')",
+        package1: "url('/media/p1.jpg')",
+        package2: "url('/media/p_2.jpeg')",
+        package3: "url('/media/p3.jpg')",
+        package4: "url('/media/p4.jpg')",
+        "5_Star_black": "url('/media/5-stars_black.png')",
+        forest: "url('/media/forest.jpg')",
+        trees: "url('/media/tree.jpg')",
+        review: "url('/media/reviews.jpg')",
+        reviewUpper1: "url('/media/reviewUpper1.jpg')",
+        reviewUpper2: "url('/media/reviewUpper2.jpeg')",
+        reviewUpper3: "url('/media/reviewUpper3.jpg')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
