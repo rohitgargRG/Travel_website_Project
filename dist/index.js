9f1accdf4532a257426600849858ca47ab8b449b
Sanchit Pahurkar <pahurkarsm@rknec.edu>
2024-06-01 17:56:32 +0530

    pushed merchandise section (responsive)

--- dist/index.js
+++ dist/index.js
@@ -9,10 +9,47 @@ burger.addEventListener("click", () => {
   }
 });
 
+const toggleButton = document.getElementById('toggleButton');
+const toggleIcon = document.getElementById('toggleIcon');
+const extraCards = document.querySelectorAll('.extra-card');
+let isExpanded = false;
+
+toggleButton.addEventListener('click', () => {
+  extraCards.forEach(card => {
+    card.classList.toggle('hidden');
+  });
+  isExpanded = !isExpanded;
+  toggleIcon.src = isExpanded ? '../media/up-arrow.jpg' : '../media/down-arrow.jpg';
+});
+
+// merchandise section
+document.addEventListener('DOMContentLoaded', function () {
+  const filterButtons = document.querySelectorAll('.filter-btn');
+  const products = document.querySelectorAll('.product');
+
+  filterButtons.forEach(button => {
+      button.addEventListener('click', () => {
+          const filter = button.getAttribute('data-filter');
+          
+          products.forEach(product => {
+              if (filter === 'all') {
+                  product.classList.remove('hidden');
+              } else {
+                  if (product.classList.contains(filter)) {
+                      product.classList.remove('hidden');
+                  } else {
+                      product.classList.add('hidden');
+                  }
+              }
+          });
+      });
+  });
+});
+
 // Scroll reveal animation
 
 document.addEventListener('DOMContentLoaded', function () {
-  ScrollReveal().reveal('.navY, .letB ,.packB', {
+  ScrollReveal().reveal('.navY, .letB ,.packB, .arrows,.productsL,.serH,.cardup', {
       origin: 'bottom',
       distance: '40px',
       duration: 1500,
@@ -21,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function () {
       reset: true
   });
 
-  ScrollReveal().reveal('.ExpT, .letL,.packL', {
+  ScrollReveal().reveal('.ExpT, .letL,.packL, .exploreR,.travelT,.serL,.logotr', {
     origin: 'left',
     distance: '50px',
     duration: 1500,
@@ -30,7 +67,7 @@ document.addEventListener('DOMContentLoaded', function () {
     reset: true
 });
 
-ScrollReveal().reveal('.ExpR,.packR', {
+ScrollReveal().reveal('.ExpR,.packR, .card,.traveltagT,.serR', {
   origin: 'right',
   distance: '50px',
   duration: 1500,
@@ -39,3 +76,6 @@ ScrollReveal().reveal('.ExpR,.packR', {
   reset: true
 });
 });
+
+
+
